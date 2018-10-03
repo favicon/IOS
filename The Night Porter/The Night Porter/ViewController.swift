e0a45defc4c3345d6835500d5179530e87092616
Kevin Kim <kevin.kim@drinks.com>
2018-10-02 17:34:47 -0700

    an initial commit

---
+++ The Night Porter/The Night Porter/ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  The Night Porter
+//
+//  Created by Kevin Kim on 10/1/18.
+//  Copyright © 2018 Kevin Kim. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBAction func changeBackground(_ sender: Any) {
+        view.backgroundColor = UIColor.darkGray
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
